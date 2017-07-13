8aa6f4c1657a4e43d4b2ee0763a8ad2ffe99325e
Heronimus <heronimustra@gmail.com>
2017-07-13 15:37:50 +0700

    WebUI update template urls views.

--- webui/urls.py
+++ webui/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^network$', views.network, name='network'),
+    url(r'^network/zones$', views.zones, name='zones'),
 ]
--- webui/views.py
+++ webui/views.py
@@ -3,7 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from settings import TEMPLATES
 
 
 def index(request):
-    return HttpResponse("Hello.")
+    return render(request, 'index.html')
+
+def network(request):
+    return render(request, 'network.html')
+
+def zones(request):
+    return render(request, 'network-manage.html')
