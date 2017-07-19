58b151f11d2b1409ec741deac3d34a67cb0557be
Heronimus <heronimustra@gmail.com>
2017-07-19 18:50:11 +0700

    Add Zone_Post API Service (WebUI)

--- webui/utils/api_service.py
+++ webui/utils/api_service.py
@@ -58,6 +58,44 @@ def delete_record(base_url_api, zone_id, record):
     response = requests.delete(base_url_api+'record/'+zone_id+'/'+record)
     return response.content
 
+def post_zones(base_url_api,named_conf,form_zone,f_zonename):
+
+    f_directive = form_zone.cleaned_data['f_directive']
+    f_authserv = form_zone.cleaned_data['f_authserv']
+    f_serialno = form_zone.cleaned_data['f_serialno']
+    f_slvrefresh = form_zone.cleaned_data['f_slvrefresh']
+    f_slvretry = form_zone.cleaned_data['f_slvretry']
+    f_slvexpire = form_zone.cleaned_data['f_slvexpire']
+    f_maxtimecache = form_zone.cleaned_data['f_maxtimecache']
+    f_adminemail = form_zone.cleaned_data['f_adminemail']
+    f_zonetype = form_zone.cleaned_data['f_zonetype']
+    # redirect to a new URL:
+    post_data = {
+        "directives": {
+            "directive1": f_directive
+        },
+        "soa_record": {
+            "authoritative_server": f_authserv,
+            "admin_email": f_adminemail,
+            "serial_no": f_serialno,
+            "slv_refresh_period": f_slvrefresh,
+            "slv_retry": f_slvretry,
+            "slv_expire": f_slvexpire,
+            "max_time_cache": f_maxtimecache
+        },
+        "zone": {
+            "zone ZONENAME": {
+                "file": f_zonename,
+                "type": f_zonetype
+            }
+        }
+    }
+
+    headers = {'content-type': 'application/json'}
+    response = requests.post(base_url_api+'zone/'+named_conf+'/', data=json.dumps(post_data), headers=headers)
+
+    return response.content
+
 def printJSONObject(data, indent=0):
     if isinstance(data, list):
         print
