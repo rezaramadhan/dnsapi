9b08fe8bca6406e5ef71661fac517dfd21ba3aed
Heronimus <heronimustra@gmail.com>
2017-07-20 18:17:36 +0700

    Update WebUI message notification

--- webui/utils/message_notif.py
+++ webui/utils/message_notif.py
@@ -2,38 +2,59 @@
 
 class MessageNotif(object):
     # The class "constructor" - It's actually an initializer
-    def __init__(self, status, message, type_notif, item):
+    def __init__(self, status, message, type_notif, message_item):
         self.status = status
         self.message = message
         self.type_notif = type_notif
-        self.item = item
+        self.message_item = message_item
 
-def get_message_notif(status, item_name='', message_custom=None):
-
-    message = 'Undefined message notification.'
+def get_message_notif(status, message_item=None, message_custom=None):
+    message_notif = {}
+    message = 'Undefined message notification. See Log.'
     type_notif = 'warning'
+
 
     if status=='success_add':
         message = 'Success Add New Record  : '
         type_notif = 'success'
     if status=='failed_add':
-        message = 'Failed Add New Record.'
+        message = 'Failed Add New Record : '
         type_notif = 'danger'
     if status=='success_edit':
         message = 'Success Edit Record  : '
         type_notif = 'success'
     if status=='failed_edit':
-        message = 'Failed Edit Record.'
+        message = 'Failed Edit Record : '
         type_notif = 'danger'
-    if status=='success_del':
+    if status=='success_delete':
         message = 'Success Delete Record  : '
         type_notif = 'danger'
-    if status=='failed_del':
-        message = 'Failed Delete Record.'
+    if status=='failed_delete':
+        message = 'Failed Delete Record : '
         type_notif = 'warning'
+    if status=='success_addzone':
+        message = 'Success Add New Zone  : '
+        type_notif = 'success'
+    if status=='failed_addzone':
+        message = 'Failed Add New Record : '
+        type_notif = 'danger'
+
+    if status=='error':
+        message = 'Error to process action : '
+        type_notif = 'danger'
+
+    # Log Link
+    if type_notif!='success':
+        message_item = message_item+'. See Log.'
+
+    # Custom Message (Info)
     if message_custom:
         message = message_custom
         type_notif = 'info'
 
+    message_notif['status'] = status
+    message_notif['message'] = message
+    message_notif['type_notif'] = type_notif
+    message_notif['message_item'] = message_item
 
-    return MessageNotif(status,message,type_notif,item_name)
+    return message_notif
