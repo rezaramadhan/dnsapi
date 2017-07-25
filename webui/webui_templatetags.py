483548861a11c73f03102646ec7f79b8bd40ac69
Heronimus <heronimustra@gmail.com>
2017-07-25 14:37:57 +0700

    Add regex filter - disable action on reverse zone

--- webui/webui_templatetags.py
+++ webui/webui_templatetags.py
@@ -11,3 +11,14 @@ def startswith(text, starts):
     if isinstance(text, basestring):
         return text.startswith(starts)
     return False
+
+@register.filter('regexmatch')
+def regexmatch(text, regex):
+
+    match = re.search(regex, text)
+    print 'debug : '+text+' - '+regex
+    print 'debug : '+str(match)
+    if match:
+        return True
+    else :
+        return False
