c4ee6e06111e59a66e14e91f8ded115cc041eccf
Diastuti Utami <13514071@std.stei.itb.ac.id>
2017-07-30 22:47:47 -0400

    Add options for slave and forward zone

--- webui/utils/zones_form.py
+++ webui/utils/zones_form.py
@@ -8,13 +8,23 @@ class RecordForm(forms.Form):
     f_priority = forms.CharField(required=False)
 
 class ZoneForm(forms.Form):
-    f_directive = forms.CharField(max_length=100)
-    f_authserv = forms.CharField(max_length=100)
-    f_serialno = forms.CharField(max_length=100)
+    # Zone Data
+    f_zonename = forms.CharField(max_length=100)
+    f_zonetype = forms.CharField(max_length=100)
+    f_zoneclass = forms.CharField(max_length=100)
+    f_zonefilename = forms.CharField(max_length=100, required=False)
+    # Forward Zone
+    f_forwarders = forms.CharField(max_length=100, required=False)
+    # Slave Zone
+    f_masters = forms.CharField(max_length=100, required=False)
+    # Master Zone
+    f_directive = forms.CharField(max_length=100, required=False)
+    f_authserv = forms.CharField(max_length=100, required=False)
+    f_serialno = forms.CharField(max_length=100, required=False)
     f_slvrefresh = forms.CharField(max_length=100, required=False)
     f_slvretry = forms.CharField(max_length=100, required=False)
     f_slvexpire = forms.CharField(max_length=100, required=False)
     f_maxtimecache = forms.CharField(max_length=100, required=False)
     f_adminemail = forms.CharField(max_length=100, required=False)
-    f_zonename = forms.CharField(max_length=100)
-    f_zonetype = forms.CharField(max_length=100)
+    # Other Statements
+    f_statement = forms.CharField(max_length=100, required=False)
