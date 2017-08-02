d1c9cce30d3b300ca82b48120f24212549fe6462
Diastuti Utami <13514071@std.stei.itb.ac.id>
2017-08-01 22:54:33 -0400

    Add log location and zone slaves

--- api/utils/config.py
+++ api/utils/config.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 REMOTE_MNT_DIR = "/"
 DEFAULT_CONF_DIR = 'etc/'
 DEFAULT_CONF_FILENAME = DEFAULT_CONF_DIR + "named.conf"
-SERVER_LIST = ["10.0.2.11", "10.0.2.1"]
+SERVER_LIST = ["10.0.2.31", "10.0.2.32"]
 
 USER_DICT = {
     SERVER_LIST[0]: "root",
@@ -17,8 +17,8 @@ USER_DICT = {
 }
 
 LOCAL_MNT_DIR = {
-    SERVER_LIST[0]: "/mnt/nfs-dns-coba1/",
-    SERVER_LIST[1]: "/mnt/nfs-dns-coba2/"
+    SERVER_LIST[0]: "/mnt/dns-local-01/",
+    SERVER_LIST[1]: "/mnt/dns-local-02/"
 }
 
 SSH_PORT = 22
@@ -30,19 +30,23 @@ IGNORED_ZONE = ['localhost', 'localhost.localdomain', '0.in-addr.arpa',
 logger.debug("Init var")
 ZONE_DICT = {}
 FILE_LOCATION = {}
-ZONE_MNT_DIR = {}
+ZONE_DIR = {}
+LOG_DIR = {}
+ZONE_SLAVES = {}
+ZONE_SOA_SERIAL = {}
 logger.debug("done init var")
 
 
 def init_data():
     """Initialize all data required in a server."""
-    global FILE_LOCATION, ZONE_DICT
+    global FILE_LOCATION, ZONE_DICT, ZONE_SLAVES
     FILE_LOCATION = {}
 
     for server in SERVER_LIST:
         ZONE_DICT[server] = []
+        ZONE_SLAVES[server] = {}
         get_zone(server, LOCAL_MNT_DIR[server] + DEFAULT_CONF_FILENAME)
-
+        get_log_location(server, LOCAL_MNT_DIR[server] + DEFAULT_CONF_FILENAME)
 
 def get_local_filename(remote_filename, relative_remote_dir, local_mnt_dir):
     """Get the mounted local filename."""
@@ -54,6 +58,14 @@ def get_local_filename(remote_filename, relative_remote_dir, local_mnt_dir):
 
     return local_filename
 
+def get_log_location(server, conf_filename):
+    """Get the log location of each server"""
+    global LOG_DIR
+    with open(conf_filename, "r") as fin:
+        conf_dict, abcd = iscpy.ParseISCString(fin.read())
+
+    log_location = ZONE_DIR[server] + conf_dict['logging']['channel default_debug']['file'].strip('"')
+    LOG_DIR[server] = log_location
 
 def get_zone(server, conf_filename, relative_remote_dir='/etc/'):
     """Read DEFAULT_CONF_FILENAME and parse the config file.
@@ -61,7 +73,7 @@ def get_zone(server, conf_filename, relative_remote_dir='/etc/'):
     This method will read all zonefile to get every zonefile available and
     also assign ZONE_DICT which server has a certain zonefile.
     """
-    global ZONE_MNT_DIR, FILE_LOCATION, ZONE_DICT
+    global ZONE_DIR, FILE_LOCATION, ZONE_DICT, ZONE_SLAVES
     logger.debug('get_zone on: ' + server + ' ' + conf_filename + ' ' + relative_remote_dir)
     with open(conf_filename, "r") as fin:
         conf_dict, abcd = iscpy.ParseISCString(fin.read())
@@ -70,7 +82,7 @@ def get_zone(server, conf_filename, relative_remote_dir='/etc/'):
         bind_working_dir = conf_dict['options']['directory'].strip('"') + '/'
     except KeyError:
         bind_working_dir = relative_remote_dir
-    ZONE_MNT_DIR[server] = bind_working_dir
+    ZONE_DIR[server] = bind_working_dir
 
     for key in conf_dict:
         if (("zone" in key) and ("master" in conf_dict[key]['type'])):
@@ -80,7 +92,15 @@ def get_zone(server, conf_filename, relative_remote_dir='/etc/'):
                 FILE_LOCATION[zone_name] = get_local_filename(conf_dict[key]['file'],
                                                               bind_working_dir,
                                                               LOCAL_MNT_DIR[server])
+
                 ZONE_DICT[server].append(zone_name)
+                ZONE_SLAVES[server][zone_name] = {}
+                if "allow-transfer" in conf_dict[key]:
+                    slaves = conf_dict[key]['allow-transfer'].keys()
+                    for slave in slaves:
+                        if slave != '':
+                            ZONE_SLAVES[server][zone_name][slave] = ""
+
         elif ("include" in key):
             # logger.log("get include :" + strconf_dict['include'])
             for (_, value) in conf_dict['include'].items():
@@ -107,4 +127,6 @@ init_data()
 # ZONE_DICT['10.0.2.11'].append('10.17.172.in-addr.arpa')
 logger.debug('FILE_LOCATION: ' + str(FILE_LOCATION))
 logger.debug('ZONE_DICT: ' + str(ZONE_DICT))
-logger.debug("ZONE_MNT_DIR: " + str(ZONE_MNT_DIR))
+logger.debug("ZONE_DIR: " + str(ZONE_DIR))
+logger.debug("LOG_DIR: " + str(LOG_DIR))
+logger.debug('ZONE_SLAVES: ' + str(ZONE_SLAVES))
